d6f10b105564a086826722bdf1a17bc6d437af77
sminlee123 <tmdals3622@naver.com>
2025-10-24 16:03:47 +0900

    refactor: nginx 추가

--- src/api/AxiosConfig.jsx
+++ src/api/AxiosConfig.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    withCredentials: true
+    baseURL: '/'
 });
 
 export default apiClient;
